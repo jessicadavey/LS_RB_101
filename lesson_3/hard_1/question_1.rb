c5267b5b125b79062cc4b74a019ff45f31dd529a
Jessica Davey <jessica@jessicadavey.com>
2020-02-19 17:47:00 -0600

    finished hard 1

---
+++ lesson_3/hard_1/question_1.rb
@@ -0,0 +1,14 @@
+if false
+  greeting = “hello world”
+end
+
+greeting
+
+# I expected this code to throw an 'undefined local variable' exception.
+
+# However, I ran it and nothing happened.
+
+# So now I'm going to do this:
+p greeting
+# And maybe the output will be 'nil'.
+# Yes, it was.
---
+++ lesson_3/hard_1/question_4.rb
@@ -0,0 +1,18 @@
+def is_an_ip_number?(number)
+  true
+end
+
+
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  return false unless dot_separated_words.size == 4
+
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    return false unless is_an_ip_number?(word)
+  end
+  true
+end
+
+
+puts dot_separated_ip_address?("10.11.banana")
