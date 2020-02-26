1991cb08ebf0d878ec2d1d7be03b0ec3783e3a86
Jessica Davey <jessica@jessicadavey.com>
2020-02-26 17:04:46 -0600

    finished lesson 4

--- lesson_4/selection_and_transformation.rb
+++ lesson_4/selection_and_transformation.rb
@@ -29,5 +29,65 @@ def select_fruit(produce)
   fruit
 end
 
+select_fruit(produce) # => {"apple"=>"Fruit", "pear"=>"Fruit"}
 
-p select_fruit(produce) # => {"apple"=>"Fruit", "pear"=>"Fruit"}
+#  Implement a double_numbers! method that mutates its argument:
+
+def double_numbers!(numbers)
+  counter = 0
+
+  loop do
+    break if counter == numbers.size
+
+    numbers[counter] *= 2
+    counter += 1
+  end
+
+  numbers
+end
+
+my_numbers = [1, 4, 3, 7, 2, 6]
+double_numbers!(my_numbers) # => [2, 8, 6, 14, 4, 12]
+
+
+# Try coding a solution that doubles the numbers that have odd indices:
+
+def double_odd_indices(numbers)
+  counter = 0
+  doubled_numbers = []
+
+  loop do
+    break if counter == numbers.size
+
+    current_number = numbers[counter]
+
+    current_number *= 2 if counter.odd?
+    doubled_numbers << current_number
+
+    counter += 1
+  end
+
+  doubled_numbers
+end
+
+my_numbers = [1, 4, 3, 7, 2, 6]
+double_odd_indices(my_numbers)  # => [1, 8, 3, 14, 2, 12]
+
+# Create a method called multiply that can take an additional 
+# argument to determine the transformation criteria.
+
+def multiply(numbers, transformation_criteria)
+  counter = 0
+  transformed_numbers = []
+
+  loop do
+    break if counter == numbers.size
+      transformed_numbers << numbers[counter] * transformation_criteria
+    counter += 1
+  end
+
+  transformed_numbers
+end
+
+my_numbers = [1, 4, 3, 7, 2, 6]
+p multiply(my_numbers, 3) # => [3, 12, 9, 21, 6, 18]
