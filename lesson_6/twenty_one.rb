4ea58752b56f804e2fbd6cf45e8f8dd7fba41717
jessicadavey <jessica@jessicadavey.com>
2020-03-09 16:13:19 +0000

    show hand method

--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -6,14 +6,30 @@ CLUB = "\xE2\x99\xA3"
 VALUES = (2..10).to_a + %w(J Q K A)
 SUITS = [SPADE, HEART, DIAMOND, CLUB]
 
-deck = VALUES.product(SUITS).shuffle
+def prompt(msg)
+  puts "=>#{msg}"
+end
+
+def show_hand(cards)
+  puts cards.map { |x| x.join }
+end
 
 def draw_a_card(deck)
   deck.pop if deck.size > 0
 end
 
-57.times do |_|
-  card = draw_a_card(deck)
-  puts card ? card.join : "no more cards"
+
+
+deck = VALUES.product(SUITS).shuffle
+player_cards = []
+
+loop do
+  show_hand(player_cards)
+  prompt "Press 'h' to hit, any other key to stay."
+  answer = gets.chomp
+  break unless  answer.chr.downcase == 'h'
+  player_cards << draw_a_card(deck)
 end
 
+prompt "Your hand:"
+show_hand(player_cards)
